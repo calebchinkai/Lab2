c0a3c4d46ea3c2fdd7582606ae2139609160771c
calebchinkai <ckcaleb.23@ichat.sp.edu.sg>
2024-04-24 14:57:55 +0800

    lab2

--- lab2.py
+++ lab2.py
@@ -1,28 +1,40 @@
+import statistics
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+
 def get_user_input():    
-    user_input=input("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    user_input=input()
     split=user_input.split(",")
     numbers_as_floats = [float(num) for num in split]
     return numbers_as_floats
 
-def calculate_statistics(temperatures):
-    # Calculate average, minimum, and maximum values
+def calc_average_temperature(temperatures):
     average_temp = sum(temperatures) / len(temperatures)
-    min_temp = min(temperatures)
-    max_temp = max(temperatures)
+    return average_temp
 
-    return average_temp, min_temp, max_temp
+def calc_min_max_temperature(temperatures):
+    min_temp=min(temperatures)
+    max_temp=max(temperatures)
+    return [min_temp,max_temp]
+
+def sort_temps(temperatures):
+    temperatures.sort()
+
+def calc_median_temperature(temperatures):
+    med_temp=statistics.median(temperatures)
+    return med_temp
 
 def main():
-    # Step 1: Get user input
+    display_main_menu()
     temperatures = get_user_input()
-
-    # Step 2: Calculate statistics
-    average_temp, min_temp, max_temp = calculate_statistics(temperatures)
-
-    # Step 3: Print results
+    average_temp=calc_average_temperature(temperatures)
+    min_temp,max_temp=calc_min_max_temperature(temperatures)
+    med_temp=calc_median_temperature(temperatures)
     print("Average temperature:", average_temp)
     print("Minimum temperature:", min_temp)
     print("Maximum temperature:", max_temp)
+    print("Median temperature: ", med_temp)
 
 if __name__ == "__main__":
     main()
